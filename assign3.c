7f1276b257dfe6fb21fd090c46960032e45299c4
RowdyRajan <rajanram@hotmail.com>
2015-04-07 09:04:55 -0600

    Not working full names

--- assign3.c
+++ assign3.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void multipleNumberInput(int spilts, int array[]);
+void waitFiveSeconds();
+void printSnapShot();
+void printResource(int resource_num);
 
 int main() {
 	int resources_num;
@@ -11,7 +15,12 @@ int main() {
 	printf("Number of different resource types: ");
 	scanf("%d", &resources_num);
 	printf("%d\n", resources_num);
-
+	printResource(resources_num);
+	printResource(resources_num);
+	printResource(resources_num);
+	printResource(resources_num);
+	printResource(resources_num);
+	printSnapShot(resources_num);
 	int resource_instances[resources_num];
 	printf("Number of instances of each resource type: ");
 	multipleNumberInput(resources_num, resource_instances);
@@ -22,28 +31,34 @@ int main() {
 		printf("%d\n", resource_instances[i]);
 	}*/
 
+	/*
 	struct procces{
-		int allocation[resources_num] = {0};
-		int request[resources_num] = {0};
+		//int allocation[resources_num] = {0};
+		//int request[resources_num] = {0};
 		int max[resources_num];
-	};
+	};*/
 
 	printf("Number of processes: ");
 	scanf("%d", &process_num);
 	printf("%d\n", process_num);
 
-	struct procces processes[process_num];
-
+	//struct procces processes[process_num];
+	int max[resources_num];
 	int process_numbers[process_num][resources_num];
 
 
 	int i = 0;
 	while(i < process_num){
 		printf("Details of P%d: ", i +1);
-		multipleNumberInput(resources_num, processes[i].max);
+		multipleNumberInput(resources_num,max);
 		i++;
 	}
 
+
+
+	while(1){
+		waitFiveSeconds();
+	}
 
 
 }
@@ -56,4 +71,30 @@ void multipleNumberInput(int resources_num, int resource_instances[]) {
 		i++;
 	}
 
+}
+void waitFiveSeconds(){
+	time_t time_start = time(NULL);
+	time_t end_time;
+	do{
+		end_time = time(NULL);
+	}while(difftime(end_time, time_start) < 5);
+
+}
+
+void printSnapShot(int resource_num){
+	printf("\n");
+	printf("%-*s", (resource_num *3),"Allocation ");
+	printf("%-*s", (resource_num *3)," request ");
+	printf("%-*s", (resource_num *3)," available ");
+	printf("%-*s", (resource_num *3)," Max Request  ");
+	printf("%-*s", (resource_num *3)," Max avialable");
+
+}
+
+void printResource(int resource_num){
+	int i = 0;
+	while( i  < resource_num){
+		printf("R%d ", i+1);
+		i++;
+	}
 }
